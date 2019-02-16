9ca9904952078f64fb89da039cc3f983fe5ef04c
Ahmed AlShalhoub <ahmedalshalhoub@Ahmeds-MacBook-Air.local>
2019-02-16 11:45:30 +0300

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -8,18 +8,37 @@ console.log("User flipped " + cardThree);
 */
 var cards= ["queen", "queen", "king","king"];
 var cardsInPlay=[];
-var cardOne= cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped " + cardOne );
-var cardTwo= cards[2];
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardTwo );
-if (cardsInPlay.length==2) {
-  if (cardsInPlay[0]===cardsInPlay[1]){
-    alert("You found a match!");
-  }
-  else {
-    alert("Sorry, try again.");
-  }
-
+function checkForMatch()
+{
+if (cardsInPlay[0] === cardsInPlay[1]) {
+alert("You found a match!");
+} else {
+alert("Sorry, try again.");
 }
+}
+function flipCard(cardId)
+{
+  //var cardOne= cards[0]; //var
+  //cardsInPlay.push(cardOne); //push
+  //console.log("User flipped " + cardOne );
+
+  //var cardTwo= cards[2]; //var
+  //cardsInPlay.push(cardTwo); //push
+  //console.log("User flipped " + cardTwo );
+ cardsInPlay.push(cards[cardId]); //push
+  // if (cardsInPlay.length==2)
+  // {
+  //   if (cardsInPlay[0]===cardsInPlay[1])
+  //   {
+  //     alert("You found a match!");
+  //   }//end small if
+  //   else
+  //   {
+  //     alert("Sorry, try again.");
+  //   }//end else
+  // }//end big IF
+checkForMatch();
+  console.log(" User flipped " + cards[cardId]);
+}//end of fucntion
+flipCard(0);
+flipCard(2);
